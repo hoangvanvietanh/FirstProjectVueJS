c48f0d3e8a90b90a72c8d1663e4396c8f8a5df37
Hoang van Viet Anh <viet_anh@lampart-vn.com>
2021-02-01 14:36:41 +0700

    asd
    asd

--- src/api/demoaxios.js
+++ src/api/demoaxios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
-// var domain = "https://hito-fake.herokuapp.com";
+ var domain = "https://hito-fake.herokuapp.com";
 // var domain = "http://172.16.16.27:4000";
-var domain = "http://hito-fake.local";
+//var domain = "http://hito-fake.local";
 function getUser( onSuccess, onError) {
      axios.
      get(`${domain}/data/user.json`)
